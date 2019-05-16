cddb6dbc9bdf57b9579d057fa1a5c8b8f7c82810
Edgar Giese <edgar@egiese.de>
2019-05-16 19:23:58 +0200

    cleanup

--- ASP.NETCoreWebAPI/Startup.cs
+++ ASP.NETCoreWebAPI/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ASP.NETCoreWebAPI.Extensions;
+﻿using ASP.NETCoreWebAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace ASP.NETCoreWebAPI
 {
@@ -54,7 +47,6 @@ namespace ASP.NETCoreWebAPI
       {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "ASP.NETCoreWebAPI v1");
       });
-
 
       app.UseMvc();
     }
